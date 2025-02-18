ebbab6774f8a1c65bf6b957d63e4b479241f47ef
Mitrayu Thakkar <tm85091@mail.harpercollege.edu>
2025-02-18 16:51:24 -0600

    Shape your destiny
    Output for Shape your destiny

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,123 @@
 package org.example;
 
 public class App {
-  public static void main(String[] args) {
-    System.out.println("You can write a driver program for your code here!");
-  }
+    public static void main(String[] args) {
+        Circle c = new Circle(5.0);
+        System.out.println("Circle area: " + c.getArea());
+        System.out.println("Circle perimeter: " + c.getPerimeter());
+
+        Rectangle r = new Rectangle(4.0, 6.0);
+        System.out.println("\nRectangle area: " + r.getArea());
+        System.out.println("Rectangle perimeter: " + r.getPerimeter());
+        System.out.println("Rectangle sides: " + r.numberOfSides());
+
+        Square s = new Square(5.0);
+        System.out.println("\nSquare area: " + s.getArea());
+        System.out.println("Square perimeter: " + s.getPerimeter());
+        System.out.println("Square sides: " + s.numberOfSides());
+
+        RightTriangle rt = new RightTriangle(3.0, 4.0);
+        System.out.println("\nRightTriangle area: " + rt.getArea());
+        System.out.println("RightTriangle perimeter: " + rt.getPerimeter());
+        System.out.println("RightTriangle sides: " + rt.numberOfSides());
+
+        IsoscelesRightTriangle iso = new IsoscelesRightTriangle(5.0);
+        System.out.println("\nIsoscelesRightTriangle area: " + iso.getArea());
+        System.out.println("IsoscelesRightTriangle perimeter: " + iso.getPerimeter());
+        System.out.println("IsoscelesRightTriangle sides: " + iso.numberOfSides());
+    }
+}
+
+abstract class Shape {
+    public abstract double getArea();
+    public abstract double getPerimeter();
+}
+
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius; // circumference
+    }
+}
+
+interface Polygon {
+    int numberOfSides();
+}
+
+class Rectangle extends Shape implements Polygon {
+    protected double length;
+    protected double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+
+    @Override
+    public int numberOfSides() {
+        return 4;
+    }
+}
+
+class RightTriangle extends Shape implements Polygon {
+    protected double legA;
+    protected double legB;
+
+    public RightTriangle(double legA, double legB) {
+        this.legA = legA;
+        this.legB = legB;
+    }
+
+    @Override
+    public double getArea() {
+        return 0.5 * legA * legB;
+    }
+
+    @Override
+    public double getPerimeter() {
+        double hypotenuse = Math.sqrt(legA * legA + legB * legB);
+        return legA + legB + hypotenuse;
+    }
+
+    @Override
+    public int numberOfSides() {
+        return 3;
+    }
+}
+
+
+class Square extends Rectangle {
+    public Square(double side) {
+        super(side, side);
+    }
+    // We do not need to override area/perimeter because
+    // they're already correct for a rectangle with equal sides.
+}
+
+class IsoscelesRightTriangle extends RightTriangle {
+    public IsoscelesRightTriangle(double leg) {
+        super(leg, leg);
+    }
 }
--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -3,10 +3,47 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
-  // You can write your unit tests here.
-  @Test
-  void appWorks() {
-    assertEquals(true, true);
-  }
+public class AppTest {
+
+    @Test
+    public void testCircle() {
+        Circle c = new Circle(5.0);
+        assertEquals(Math.PI * 25, c.getArea(), 0.0001);
+        assertEquals(Math.PI * 10, c.getPerimeter(), 0.0001);
+    }
+
+    @Test
+    public void testRectangle() {
+        Rectangle r = new Rectangle(4.0, 6.0);
+        assertEquals(24.0, r.getArea(), 0.0001);
+        assertEquals(20.0, r.getPerimeter(), 0.0001);
+        assertEquals(4, r.numberOfSides());
+    }
+
+    @Test
+    public void testSquare() {
+        Square s = new Square(5.0);
+        assertEquals(25.0, s.getArea(), 0.0001);
+        assertEquals(20.0, s.getPerimeter(), 0.0001);
+        assertEquals(4, s.numberOfSides());
+    }
+
+    @Test
+    public void testRightTriangle() {
+        RightTriangle rt = new RightTriangle(3.0, 4.0);
+        // area = 0.5 * 3 * 4 = 6.0
+        assertEquals(6.0, rt.getArea(), 0.0001);
+        assertEquals(12.0, rt.getPerimeter(), 0.0001);
+        assertEquals(3, rt.numberOfSides());
+    }
+
+    @Test
+    public void testIsoscelesRightTriangle() {
+        IsoscelesRightTriangle irt = new IsoscelesRightTriangle(5.0);
+        // area = 0.5 * 5 * 5 = 12.5
+        assertEquals(12.5, irt.getArea(), 0.0001);
+        double expected = 10 + Math.sqrt(50);
+        assertEquals(expected, irt.getPerimeter(), 0.0001);
+        assertEquals(3, irt.numberOfSides());
+    }
 }
